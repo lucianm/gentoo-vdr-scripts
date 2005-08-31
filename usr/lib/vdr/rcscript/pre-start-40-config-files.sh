7cae63aebff9562474debd7e1bcfaee3a96e0064
Matthias Schwarzott <zzam@gentoo.org>
2005-08-31 16:36:21 +0000

    added setting of startup volume and channel
    svn path=/gentoo-vdr-scripts/trunk/; revision=9

--- usr/lib/vdr/rcscript/pre-start-40-config-files.sh
+++ usr/lib/vdr/rcscript/pre-start-40-config-files.sh
@@ -44,4 +44,15 @@ if [[ ! -d /var/vdr ]]; then
 fi
 create_commands_conf commands "${ORDER_COMMANDS}"
 create_commands_conf reccmds "${ORDER_RECCMDS}"
+
+if [[ -f /etc/vdr/setup.conf ]]; then
+	if [[ -n "${STARTUP_VOLUME}" ]]; then
+		/usr/bin/sed -i /etc/vdr/setup.conf -e "s/^CurrentVolume =.*\$/CurrentVolume = ${STARTUP_VOLUME}/"
+	fi
+
+	if [[ -n "${STARTUP_CHANNEL}" ]]; then
+		/usr/bin/sed -i /etc/vdr/setup.conf -e "s/^CurrentChannel =.*\$/CurrentChannel = ${STARTUP_CHANNEL}/"
+	fi
+fi
+
 eend 0
