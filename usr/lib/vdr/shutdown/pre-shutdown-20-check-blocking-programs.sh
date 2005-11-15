0cab86e18e8f012b6babdcf2b8df28b8a0e471a9
Matthias Schwarzott <zzam@gentoo.org>
2005-11-15 22:27:01 +0000

    Enable force of shutdown for running programs
    svn path=/gentoo-vdr-scripts/trunk/; revision=73

--- usr/lib/vdr/shutdown/pre-shutdown-20-check-blocking-programs.sh
+++ usr/lib/vdr/shutdown/pre-shutdown-20-check-blocking-programs.sh
@@ -1,10 +1,11 @@
-SHUTDOWN_CHECK_PROGRAMS="make gcc noad cc1 transcode vcdimager mencoder tosvcd lame vdrsync.pl tcmplex"
+SHUTDOWN_CHECK_PROGRAMS="emerge make gcc noad cc1 transcode vcdimager mencoder tosvcd lame vdrsync.pl tcmplex"
 SHUTDOWN_CHECK_PROGRAMS="${SHUTDOWN_CHECK_PROGRAMS} tcmplex-panteltje vdr2ac3.sh"
 
 PIDOF=pidof
 for PROG in ${SHUTDOWN_CHECK_PROGRAMS} ${SHUTDOWN_CHECK_ADDITIONAL_PROGRAMS}; do
 	if ${PIDOF} -x ${PROG} >/dev/null; then
 		# stop shutdown
-		shutdown_abort "${PROG} is running"
+		shutdown_abort_can_force "${PROG} is running"
+		break
 	fi
 done
