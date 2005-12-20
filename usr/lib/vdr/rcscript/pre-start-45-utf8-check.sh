b76ce9b95c888258e3f8a05501f8364c8afb3c61
Matthias Schwarzott <zzam@gentoo.org>
2005-12-20 21:50:34 +0000

    added utf8-workaround
    svn path=/gentoo-vdr-scripts/trunk/; revision=85

---
+++ usr/lib/vdr/rcscript/pre-start-45-utf8-check.sh
@@ -0,0 +1,11 @@
+addon_main() {
+	[[ -n ${CAP_UTF8} ]] && return
+
+	local lang_vars=$(locale)
+	[[ ${lang_vars} != ${lang_vars//utf8/} ]] && einfo_level1 "disabling UTF-8 for vdr"
+	# deaktiviere utf8
+	for lvar in ${lang_vars}; do
+		eval export ${lvar/.utf8/}
+	done
+}
+
