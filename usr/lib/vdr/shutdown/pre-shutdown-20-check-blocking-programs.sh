6aee9eaf93ff424d9f91afecfc632346a6c7ca9d
Matthias Schwarzott <zzam@gentoo.org>
2005-12-24 17:05:56 +0000

    added dvdauthor to list of shutdown-blocking programs
    svn path=/gentoo-vdr-scripts/trunk/; revision=91

--- usr/lib/vdr/shutdown/pre-shutdown-20-check-blocking-programs.sh
+++ usr/lib/vdr/shutdown/pre-shutdown-20-check-blocking-programs.sh
@@ -1,5 +1,5 @@
 SHUTDOWN_CHECK_PROGRAMS="emerge make gcc noad cc1 transcode vcdimager mencoder tosvcd lame vdrsync.pl tcmplex"
-SHUTDOWN_CHECK_PROGRAMS="${SHUTDOWN_CHECK_PROGRAMS} tcmplex-panteltje vdr2ac3.sh"
+SHUTDOWN_CHECK_PROGRAMS="${SHUTDOWN_CHECK_PROGRAMS} tcmplex-panteltje vdr2ac3.sh dvdauthor"
 
 PIDOF=pidof
 for PROG in ${SHUTDOWN_CHECK_PROGRAMS} ${SHUTDOWN_CHECK_ADDITIONAL_PROGRAMS}; do
