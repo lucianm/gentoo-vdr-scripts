4f0edd059f6c90873750ded80900c3fabd72cc61
Matthias Schwarzott <zzam@gentoo.org>
2005-12-27 17:17:08 +0000

    added more shutdown-blockers
    svn path=/gentoo-vdr-scripts/trunk/; revision=92

--- usr/lib/vdr/shutdown/pre-shutdown-20-check-blocking-programs.sh
+++ usr/lib/vdr/shutdown/pre-shutdown-20-check-blocking-programs.sh
@@ -1,5 +1,6 @@
 SHUTDOWN_CHECK_PROGRAMS="emerge make gcc noad cc1 transcode vcdimager mencoder tosvcd lame vdrsync.pl tcmplex"
-SHUTDOWN_CHECK_PROGRAMS="${SHUTDOWN_CHECK_PROGRAMS} tcmplex-panteltje vdr2ac3.sh dvdauthor"
+SHUTDOWN_CHECK_PROGRAMS="${SHUTDOWN_CHECK_PROGRAMS} tcmplex-panteltje vdr2ac3.sh dvdauthor mkisofs"
+SHUTDOWN_CHECK_PROGRAMS="${SHUTDOWN_CHECK_PROGRAMS} growisofs cdrdao cdrecord"
 
 PIDOF=pidof
 for PROG in ${SHUTDOWN_CHECK_PROGRAMS} ${SHUTDOWN_CHECK_ADDITIONAL_PROGRAMS}; do
