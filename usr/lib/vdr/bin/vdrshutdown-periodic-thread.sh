8f486fc8734d3df4bf023202a80193270d86d14f
Matthias Schwarzott <zzam@gentoo.org>
2005-12-30 15:02:04 +0000

    base time-calculation on thread end-time, not start-time
    svn path=/gentoo-vdr-scripts/trunk/; revision=105

--- usr/lib/vdr/bin/vdrshutdown-periodic-thread.sh
+++ usr/lib/vdr/bin/vdrshutdown-periodic-thread.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 source /usr/lib/vdr/rcscript/functions-shutdown.sh
 THREAD_DIR=/usr/lib/vdr/shutdown
+PERIODIC_THREAD_ENDTIME=${shutdown_data_dir}/periodic_thread_last_ended
 
 mesg "periodic thread started"
 
 for HOOK in ${THREAD_DIR}/periodic-*.sh; do
 	[[ -f "${HOOK}" ]] && source "${HOOK}"
 done
+
+NOW=$(date +%s)
+echo ${NOW} > ${PERIODIC_THREAD_ENDTIME}
+
 mesg "periodic thread finished"
 
 sleep 3s
--- usr/lib/vdr/shutdown/pre-shutdown-99-periodic-thread.sh
+++ usr/lib/vdr/shutdown/pre-shutdown-99-periodic-thread.sh
@@ -1,4 +1,4 @@
-PERIODIC_THREAD_STARTTIME=${shutdown_data_dir}/periodic_thread_last_started
+PERIODIC_THREAD_ENDTIME=${shutdown_data_dir}/periodic_thread_last_ended
 
 
 check_periodic_thread()
@@ -19,15 +19,14 @@ check_periodic_thread()
 	local NOW=$(date +%s)
 	local MINIMAL_THREAD_CALL_DELTA=$(( 3600*20 ))
 
-	local LAST_THREAD_START=0
-	[[ -f ${PERIODIC_THREAD_STARTTIME} ]] && LAST_THREAD_START=$(<${PERIODIC_THREAD_STARTTIME})
+	local LAST_THREAD_END=0
+	[[ -f ${PERIODIC_THREAD_ENDTIME} ]] && LAST_THREAD_END=$(<${PERIODIC_THREAD_ENDTIME})
 	
-	local DELTA=$(( NOW-LAST_THREAD_START ))
+	local DELTA=$(( NOW-LAST_THREAD_END ))
 
 	[[ ${DELTA} -lt ${MINIMAL_THREAD_CALL_DELTA} ]] && return
 	
 	# can take longer time
-	echo ${NOW} > ${PERIODIC_THREAD_STARTTIME}
 	/usr/lib/vdr/bin/vdr-bg.sh /usr/lib/vdr/shutdown/vdrshutdown-periodic-thread.sh &
 	
 	exit 0
