acce3e11d5daecdbe9339889369a426fbf8763b0
Matthias Schwarzott <zzam@gentoo.org>
2006-02-28 21:10:52 +0000

    do not print message about not defined function in addon
    svn path=/gentoo-vdr-scripts/trunk/; revision=175

--- usr/lib/vdr/inc/rc-functions.sh
+++ usr/lib/vdr/inc/rc-functions.sh
@@ -79,7 +79,7 @@ load_addon()
 {
 	local addon=${1}
 	local call_func=${2:-addon_main}
-	eval "${call_func}() { einfo_level2 called undefined ${call_func} for ${addon}; }"
+	eval "${call_func}() { :; }"
 
 	# source addon
 	if [[ -f "${addon}" ]]; then
