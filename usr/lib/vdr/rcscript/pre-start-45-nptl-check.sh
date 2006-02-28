8cb1aafb7450c92df7cf1b121da6332ce17f1c1b
Matthias Schwarzott <zzam@gentoo.org>
2006-02-28 21:00:05 +0000

    changed debug-message for NPTL-off to be less confusing
    svn path=/gentoo-vdr-scripts/trunk/; revision=173

--- usr/lib/vdr/rcscript/pre-start-45-nptl-check.sh
+++ usr/lib/vdr/rcscript/pre-start-45-nptl-check.sh
@@ -4,7 +4,7 @@ addon_main() {
 		# Test wheather force of old pthreads will work
 		if LD_ASSUME_KERNEL=2.4.1 /bin/true 2>/dev/null; then
 			export LD_ASSUME_KERNEL=2.4.1
-			einfo_level2 setting LD_ASSUME_KERNEL=2.4.1
+			einfo_level2 "Forcing NPTL off"
 		else
 			einfo "vdr: You use a NPTL only system, using nptl and hoping the best."
 		fi
