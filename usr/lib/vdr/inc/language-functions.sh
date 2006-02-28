8e3e48b58e1cc54fa988ca7c5bedb2ccff7b40a3
Matthias Schwarzott <zzam@gentoo.org>
2006-02-28 21:09:22 +0000

    only read setup.conf if existing
    svn path=/gentoo-vdr-scripts/trunk/; revision=174

--- usr/lib/vdr/inc/language-functions.sh
+++ usr/lib/vdr/inc/language-functions.sh
@@ -2,7 +2,11 @@
 read_vdr_language() {
 	local OSDLANG
 	local LANG_TAB
-	OSDLANG=$(awk -F= '/^OSDLanguage/ { print $2 }' /etc/vdr/setup.conf)
+	if [[ -f /etc/vdr/setup.conf ]]; then
+		OSDLANG=$(awk -F= '/^OSDLanguage/ { print $2 }' /etc/vdr/setup.conf)
+	else
+		OSDLANG="0"
+	fi
 	LANG_TAB=("en" "de")
 	VDR_LANGUAGE=${LANG_TAB[OSDLANG]}
 }
