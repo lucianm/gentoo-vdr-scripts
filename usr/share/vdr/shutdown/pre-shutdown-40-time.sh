755ca98e195b8fe2e4f35bd456f734cc491bbf57
Matthias Schwarzott <zzam@gentoo.org>
2006-06-16 11:41:15 +0000

    Bugfix forbidden-times shutdown stopper. Reported by Roland May <tinitus@web.de>.
    svn path=/gentoo-vdr-scripts/trunk/; revision=287

--- usr/share/vdr/shutdown/pre-shutdown-40-time.sh
+++ usr/share/vdr/shutdown/pre-shutdown-40-time.sh
@@ -3,8 +3,9 @@
 do_check_time() {
 	if [[ -n ${BLOCK_SHUTDOWN_INTERVALS} ]]; then
 		include time
-		check_interval NOW "${BLOCK_SHUTDOWN_INTERVALS}"
-		shutdown_abort "shutdown forbidden time"
+		if check_interval NOW "${BLOCK_SHUTDOWN_INTERVALS}"; then
+			shutdown_abort "shutdown forbidden time"
+		fi
 	fi
 }
 
