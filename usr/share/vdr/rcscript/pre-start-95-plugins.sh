18a1035ccc57069a478ea326681fec28a6c7b2ed
Matthias Schwarzott <zzam@gentoo.org>
2006-10-18 16:55:37 +0000

    Moved the reader for new plugin-conf-system to correct place, as then only start-rcaddons will work.
    svn path=/gentoo-vdr-scripts/trunk/; revision=336

--- usr/share/vdr/rcscript/pre-start-95-plugins.sh
+++ usr/share/vdr/rcscript/pre-start-95-plugins.sh
@@ -7,24 +7,6 @@ addon_main() {
 	local SKIP_COUNT=0
 	local PLUGIN_COUNT=0
 
-	# old conf-system - PLUGINS in /etc/conf.d/vdr
-	# now this gets cleared in here
-	PLUGINS=""
-
-	# new conf-system - /etc/conf.d/vdr.plugins
-	local PLUGIN_CONF=/etc/conf.d/vdr.plugins
-	if [[ -f ${PLUGIN_CONF} ]]; then
-		local line
-		exec 3<${PLUGIN_CONF}
-		while read -u 3 line; do
-			[[ ${line} == "" ]] && continue
-			[[ ${line:0:1} == "#" ]] && continue
-			PLUGIN="${line}"
-			PLUGINS="${PLUGINS} ${PLUGIN}"
-		done
-		exec 3<&-
-	fi
-
 	for PLUGIN in ${PLUGINS}; do
 		: $(( PLUGIN_COUNT++ ))
 
