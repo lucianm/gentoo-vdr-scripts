f95a94f68fd489d4927bd66a1ebbd4868119fbee
Matthias Schwarzott <zzam@gentoo.org>
2007-01-10 18:25:52 +0000

    No longer hard disable shutdown if there are things todo, warn only now.
    svn path=/gentoo-vdr-scripts/trunk/; revision=378

--- usr/share/vdr/rcscript/pre-start-50-shutdown.sh
+++ usr/share/vdr/rcscript/pre-start-50-shutdown.sh
@@ -13,10 +13,6 @@ list_wakeup_methods() {
 	vdr_einfo "    You have to reemerge gentoo-vdr-scripts with the new use-flags set."
 }
 
-shutdown_disabled() {
-	vdr_eerror "  Disabled shutdown!"
-}
-
 addon_main() {
 	include shutdown-functions
 	[[ "${SHUTDOWN_ACTIVE}" == "no" ]] && return 0
@@ -27,18 +23,17 @@ addon_main() {
 	fi
 
 	# no custum shutdown-script
+	add_param "--shutdown=/usr/share/vdr/bin/vdrshutdown-gate.sh"
 
-	# test for good sudo-configuration
+	# some sanity warnings
 	if ! grep -q /usr/share/vdr/bin/vdrshutdown-really.sh /etc/sudoers; then
 		vdr_ewarn "  missing entry in /etc/sudoers"
-		vdr_einfo
-		vdr_einfo "  please add this line to your /etc/sudoers file"
-		vdr_einfo "  vdr ALL=NOPASSWD:/usr/share/vdr/bin/vdrshutdown-really.sh"
-		vdr_einfo
-		vdr_einfo "  or call emerge --config gentoo-vdr-scripts"
-		vdr_einfo
-		shutdown_disabled
-		return 0
+		vdr_ewarn
+		vdr_ewarn "  please add this line to your /etc/sudoers file"
+		vdr_ewarn "  vdr ALL=NOPASSWD:/usr/share/vdr/bin/vdrshutdown-really.sh"
+		vdr_ewarn
+		vdr_ewarn "  or call emerge --config gentoo-vdr-scripts"
+		vdr_ewarn
 	fi
 
 	if [[ -f ${shutdown_script_dir}/wakeup-${WAKEUP_METHOD}.sh ]]; then
@@ -47,17 +42,12 @@ addon_main() {
 		# test if needed programs are there
 		if ! wakeup_check; then
 			list_wakeup_methods
-			shutdown_disabled
-			return 0
 		fi
 	else
 		vdr_eerror "  Wakeup-Method ${WAKEUP_METHOD} not supported!"
 		list_wakeup_methods
-		shutdown_disabled
-		return 0
 	fi
 
-	add_param "--shutdown=/usr/share/vdr/bin/vdrshutdown-gate.sh"
 	return 0
 }
 
