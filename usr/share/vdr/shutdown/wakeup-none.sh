af7cfdb75e34f4eab6cb4f0b887cd0709635a506
Matthias Schwarzott <zzam@gentoo.org>
2007-01-13 17:22:42 +0000

    Let code use yes/no like conf-file suggested already.
    svn path=/gentoo-vdr-scripts/trunk/; revision=395

--- usr/share/vdr/shutdown/wakeup-none.sh
+++ usr/share/vdr/shutdown/wakeup-none.sh
@@ -14,7 +14,7 @@ wakeup_check() {
 wakeup_set() {
 	if [[ ${1} != 0 ]]; then
 		# it is not possible to wakeup the system!
-		if [[ ${NONE_WAKEUP_IGNORE_TIMER:-0} == 1 ]]; then
+		if [[ ${NONE_WAKEUP_IGNORE_TIMER:-no} == yes ]]; then
 			# ignoring set timers
 			:
 		else
