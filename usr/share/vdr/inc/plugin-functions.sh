8d48037a4d3da2273f77bc884c193e9f5aeaf7c7
Matthias Schwarzott <zzam@gentoo.org>
2007-01-21 21:54:54 +0000

    Do no longer call check_plugin when stopping to not modify list of plugins.
    svn path=/gentoo-vdr-scripts/trunk/; revision=398

--- usr/share/vdr/inc/plugin-functions.sh
+++ usr/share/vdr/inc/plugin-functions.sh
@@ -14,73 +14,68 @@
 
 
 init_plugin_loader() {
-	PLUGIN_CHECK_MD5=no
-
-	if ! which md5sum >/dev/null 2>&1; then
-		return
-	fi
-
 	if [[ ! -d /var/vdr/tmp ]]; then
 		mkdir /var/vdr/tmp
 		chown vdr:vdr /var/vdr/tmp
 	fi
-
 
 	plugin_dir=$(awk '/^PLUGINLIBDIR/{ print $3 }' /usr/include/vdr/Make.config)
 	if [[ -n ${plugin_dir} ]]; then
 		plugin_dir=/usr/lib/vdr/plugins
 	fi
 
-	vdr_checksum_dir="${plugin_dir%/plugins}/checksums"
-	vdr_checksum=${vdr_checksum_dir}/header-md5-vdr
+	if which md5sum >/dev/null 2>&1; then
+		vdr_checksum_dir="${plugin_dir%/plugins}/checksums"
+		vdr_checksum=${vdr_checksum_dir}/header-md5-vdr
 
-	if [[ ! -f ${vdr_checksum} ]]; then
-		vdr_checksum=/var/vdr/tmp/header-md5-vdr
+		if [[ ! -f ${vdr_checksum} ]]; then
+			vdr_checksum=/var/vdr/tmp/header-md5-vdr
 
-		rm ${vdr_checksum} 2>/dev/null
-		(
-			cd /usr/include/vdr
-			md5sum *.h libsi/*.h|LC_ALL=C sort --key=2
-		) > ${vdr_checksum}
+			rm ${vdr_checksum} 2>/dev/null
+			(
+				cd /usr/include/vdr
+				md5sum *.h libsi/*.h|LC_ALL=C sort --key=2
+			) > ${vdr_checksum}
+		fi
+		PLUGIN_CHECK_MD5=yes
+	else
+		PLUGIN_CHECK_MD5=no
 	fi
 
-	PLUGIN_CHECK_MD5=yes
 
-
+	# Loading configuration
 
 	# old conf-system - PLUGINS in /etc/conf.d/vdr
 	# now this gets cleared in here
 	PLUGINS=""
 
 	# Load list of plugins which were started to exec correct rcaddons
-	local LOADED_PLUGINS=/var/vdr/tmp/loaded_plugins
-	if [[ ${INIT_PHASE} == "stop" && -e ${LOADED_PLUGINS} ]]; then
-		PLUGINS=$(< ${LOADED_PLUGINS} )
-		return
+	local LOADED_PLUGINS_FILE=/var/vdr/tmp/loaded_plugins
+	if [[ ${INIT_PHASE} == "stop" && -e ${LOADED_PLUGINS_FILE} ]]; then
+		PLUGINS=$(< ${LOADED_PLUGINS_FILE} )
 	else
-		rm -f ${LOADED_PLUGINS}
-	fi
+		rm -f ${LOADED_PLUGINS_FILE}
 
-	# new conf-system - /etc/conf.d/vdr.plugins
-	local PLUGIN_CONF=/etc/conf.d/vdr.plugins
-	if [[ -f ${PLUGIN_CONF} ]]; then
-		local line
-		exec 3<${PLUGIN_CONF}
-		while read -u 3 line; do
-			[[ ${line} == "" ]] && continue
-			[[ ${line:0:1} == "#" ]] && continue
-			PLUGIN="${line}"
-			PLUGINS="${PLUGINS} ${PLUGIN}"
-		done
-		exec 3<&-
-	fi
+		# new conf-system - /etc/conf.d/vdr.plugins
+		local PLUGIN_CONF=/etc/conf.d/vdr.plugins
+		if [[ -f ${PLUGIN_CONF} ]]; then
+			local line
+			exec 3<${PLUGIN_CONF}
+			while read -u 3 line; do
+				[[ ${line} == "" ]] && continue
+				[[ ${line:0:1} == "#" ]] && continue
+				PLUGIN="${line}"
+				PLUGINS="${PLUGINS} ${PLUGIN}"
+			done
+			exec 3<&-
+		fi
 
-	# Store list of loaded plugins
-	echo ${PLUGINS} > ${LOADED_PLUGINS}
+		# Store list of loaded plugins
+		echo ${PLUGINS} > ${LOADED_PLUGINS_FILE}
+	fi
 }
 
 check_plugin() {
-	SKIP_PLUGIN=0
 	local PLUGIN="${1}"
 	local plugin_file="${plugin_dir}/libvdr-${PLUGIN}.so.${APIVERSION}"
 
@@ -107,8 +102,12 @@ load_plugin()
 {
 	local PLUGIN="${1}"
 	local call_func="${2}"
+	SKIP_PLUGIN=0
 
-	check_plugin ${PLUGIN}
+	# Only check when starting vdr
+	if [[ ${INIT_PHASE} != "stop" ]]; then
+		check_plugin ${PLUGIN}
+	fi
 	[[ "${SKIP_PLUGIN}" == "1" ]] && return
 
 	unset _EXTRAOPTS
