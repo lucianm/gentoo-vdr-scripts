7a0e63e4924accfddce241845a56553aa2c5bfd8
Matthias Schwarzott <zzam@gentoo.org>
2007-07-10 13:51:31 +0000

    Does not only show nice plugin failures, but also nicify the other errors.
    svn path=/gentoo-vdr-scripts/trunk/; revision=484

--- usr/share/vdr/rcscript/post-start-60-check-syslog-errors.sh
+++ usr/share/vdr/rcscript/post-start-60-check-syslog-errors.sh
@@ -1,18 +1,30 @@
-addon_main() {
-	# simulate an error
-	#logger "DUMMY-ERROR: /usr/lib/vdr/plugins/libvdr-softdevice.so.1.5.5: undefined symbol: _Z14pp_postprocessPPhPiS0_S1_iiPaiPvS3_i"
 
+simulate_syslog_errors() {
+	# Inject some errors to test the code
+	logger -t vdr "DUMMY ERROR: /usr/lib/vdr/plugins/libvdr-softdevice.so.1.5.5: undefined symbol: _Z14pp_postprocessPPhPiS0_S1_iiPaiPvS3_i"
+	logger -t vdr "ERROR: unknown plugin 'director'"
+	logger -t vdr "ERROR: empty key macro"
+	logger -t vdr "ERROR: source base /net/gauss/home/audio not found"
+	logger -t vdr "ERROR: source base /mnt/usbstick not found"
+}
+
+addon_main() {
 	# Abort if we did not find a syslog file
 	[ ! -e "${SYSLOG_FILE}" ] && return 0
+
+	# simulate errors
+	#simulate_syslog_errors
 
 	#einfo Checking for errors in syslog
 	local line
 	local count=0
 
+	# extract relevant error lines out of syslog, and show up to 5 of them
 	sed "${SYSLOG_LINES}"',$!d
-		/ERROR/!d
+		/vdr.*ERROR/!d
+		s/^.* ERROR: /ERROR: /
 		/unknown config parameter:/d
-		/ERROR.*lib\/vdr\/plugins/ { s/^.*ERROR:.*\/plugins\//Error loading plugin / }
+		s#ERROR: /usr/lib/vdr/plugins/#ERROR: loading plugin #
 		' "${SYSLOG_FILE}" \
 	| while read line; do
 		count=$(($count+1))
