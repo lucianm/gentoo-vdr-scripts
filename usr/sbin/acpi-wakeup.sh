f9b734add3412cb80992e46037b31c19611bfb9d
Matthias Schwarzott <zzam@gentoo.org>
2007-10-16 08:55:18 +0000

    Updated acpi-wakeup to also work with new rtc-interface.
    svn path=/gentoo-vdr-scripts/trunk/; revision=523

--- usr/sbin/acpi-wakeup.sh
+++ usr/sbin/acpi-wakeup.sh
@@ -14,12 +14,14 @@
 #
 
 PROC_ALARM="/proc/acpi/alarm"
+RTC_ALARM="/sys/class/rtc/rtc0/wakealarm"
+
 #DEBUG=1
 [ -z "$DEBUG" ] && DEBUG=0
 
 die()
 {
-	echo $@ 1>&2
+	echo "ERROR: $@" 1>&2
 	exit 1
 }
 
@@ -31,7 +33,7 @@ checkUTC()
 	else
 		CLOCK=$(. /etc/rc.conf; echo ${CLOCK})
 	fi
-	[ "${DEBUG}" = "1" ] && echo CLOCK-MODE: ${CLOCK}
+	[ "${CLOCK}" = "UTC" ]
 }
 
 saveTime()
@@ -43,39 +45,54 @@ saveTime()
 
 setAlarm()
 {
-	checkUTC
-	local timestr
+	local timestamp
 	if [ "${Next}" -gt 0 ]; then
 		# abort if recording less then 10min in future
 		local now=$(date +%s)
 		[ "${Next}" -lt "$(($now+600))" ] && die "wakeup time too near, alarm not set"
 
 		# boot 5min (=300s) before recording
-		local t=$(($Next-300))
-
-		[ "${CLOCK}" = "UTC" ] && dateparam="-u"
-		timestr=$(date ${dateparam} --date="1970-01-01 UTC ${t} seconds" +"%Y-%m-%d %H:%M:00")
+		timestamp=$(($Next-300))
 	else
-		# This hopefully deactivates wakeup
-		timestr="2003-10-20 99:00:00"
+		timestamp=0
 	fi
 
-	[ -z "${timestr}" ] && return
-
+	# write time to RTC now, as it may disable wakeup if done after writing alarm time
 	saveTime
 
-	echo ${timestr} > ${PROC_ALARM} 
-	echo ${timestr} > ${PROC_ALARM} 
+	# new interface Kernel 2.6.22+
+	if [ -e ${RTC_ALARM} ]; then
+		local timestr=${timestamp}
+		# maybe this needs to be adjusted if bios time is not in UTC
 
-	if [ "${DEBUG}" = "1" ]; then
-		echo ${timestr} > /tmp/a
-		echo Wakeup at ${timestr} >> /tmp/a
+		# clear old time
+		echo 0 > ${RTC_ALARM}
+		echo "${timestr}" > ${RTC_ALARM}
+		return
 	fi
+		
+	# old interface
+	if [ -e ${PROC_ALARM} ]; then
+		# This hopefully deactivates wakeup
+		local timestr="2003-10-20 99:00:00"
+
+		if [ ${timestamp} -gt 0 ]; then
+			checkUTC && dateparam="-u"
+			timestr=$(date ${dateparam} --date="1970-01-01 UTC ${timestamp} seconds" +"%Y-%m-%d %H:%M:00")
+			[ -z "${timestr}" ] && die "date did not return a string"
+		fi
+
+		# write 2 times
+		echo ${timestr} > ${PROC_ALARM}
+		echo ${timestr} > ${PROC_ALARM}
+		return
+	fi
+
+	die "Kernel does not support ACPI alarm"
 }
 
 
 
-test -f "${PROC_ALARM}" || die "${PROC_ALARM} missing"
 test $# -ge 1 || die "Wrong Parameter Count"
 Next="${1}"
 Delta="${2}"
