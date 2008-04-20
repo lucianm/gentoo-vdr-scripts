7e834e20dc08953719da9e2ad071aa05bfd742ea
Matthias Schwarzott <zzam@gentoo.org>
2008-04-20 19:42:28 +0000

    Fix bump.sh
    svn path=/gentoo-vdr-scripts/trunk/; revision=595

--- bump.sh
+++ bump.sh
@@ -33,5 +33,7 @@ svn commit -m "Bumped to version ${NEWVERS}" ChangeLog README
 cd ..
 echo "svn copy"
 svn copy trunk "tags/${NEWVERS}"
-svn commit -m "Tagged version ${NEWVERS}" tags/${NEWVERS}"
+svn commit -m "Tagged version ${NEWVERS}" "tags/${NEWVERS}"
+cd "tags/${NEWVERS}"
+make dist
 
