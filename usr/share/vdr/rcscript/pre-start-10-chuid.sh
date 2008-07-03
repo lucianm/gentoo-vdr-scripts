add7a6a5ead4f3500fcbb6fca44601576bfa36fc
Matthias Schwarzott <zzam@gentoo.org>
2008-07-03 21:07:36 +0000

    Add config-setting to disable usage of the vdr -u option.
    svn path=/gentoo-vdr-scripts/trunk/; revision=621

--- usr/share/vdr/rcscript/pre-start-10-chuid.sh
+++ usr/share/vdr/rcscript/pre-start-10-chuid.sh
@@ -1,7 +1,7 @@
 # $Id$
 addon_main() {
 	if [ "${START_VDR_AS_ROOT}" != "YES" ]; then
-		if [ -n "${CAP_CHUID}" ]; then
+		if [ -n "${CAP_CHUID}" -a "${FORCE_SETUID_OFF:-no}" = "no" ]; then
 			add_param "-u" "vdr"
 		else
 			add_daemonctrl_param --chuid vdr
