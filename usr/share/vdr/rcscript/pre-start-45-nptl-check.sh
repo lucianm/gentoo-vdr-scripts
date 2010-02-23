1ac545966fa692e8727829f3785449e60e0661bd
Matthias Schwarzott <zzam@gentoo.org>
2010-02-23 21:14:29 +0000

    Change default of FORCE_OLD_THREADS to no. Suggested by chub in #gentoo-vdr.
    svn path=/gentoo-vdr-scripts/trunk/; revision=665

--- usr/share/vdr/rcscript/pre-start-45-nptl-check.sh
+++ usr/share/vdr/rcscript/pre-start-45-nptl-check.sh
@@ -1,6 +1,6 @@
 # $Id$
 addon_main() {
-	if yesno "${FORCE_OLD_THREADS:-yes}"; then
+	if yesno "${FORCE_OLD_THREADS:-no}"; then
 		# Test wheather force of old pthreads will work
 		if LD_ASSUME_KERNEL=2.4.1 /bin/true 2>/dev/null; then
 			export LD_ASSUME_KERNEL=2.4.1
