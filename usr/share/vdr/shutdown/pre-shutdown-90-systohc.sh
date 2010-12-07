3b3117569c9c46201537ff8cc777dc9ddeafdf8b
Jörg Bornkessel <hd_brummy@gentoo.org>
2010-12-07 01:14:08 +0000

    added shutdown fix for probs on wrong time with used system systohc, bug #263956
    svn path=/gentoo-vdr-scripts/trunk/; revision=673

---
+++ usr/share/vdr/shutdown/pre-shutdown-90-systohc.sh
@@ -0,0 +1,24 @@
+# $Id: $
+
+if [ ! -f /etc/init.d/sysfs ]; then
+# Baselayout 1
+. /etc/conf.d/clock
+    else
+# Baselayout 2
+. /etc/conf.d/hwclock
+fi
+
+
+do_systohc() {
+    hwclock --systohc
+}
+
+if yesno "${SHUTDOWN_SYSTOHC:-no}"; then
+
+    if [ ${CLOCK_SYSTOHC:=no} == "no" ] || [ ${clock_systohc:=NO} == "NO" ] ;then
+
+        do_systohc
+
+    fi
+
+fi
